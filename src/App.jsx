0f521ca702747e5dc22b26964f3e2d8350226f6e
anggakusumap <kusuma_putra@student.unud.ac.id>
2024-06-08 07:24:01 +0800

    Develop delete todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { useState } from 'react'
 import Todos from './components/Todos';
 
 const data = [
-    {
-      id: 1,
-      title: 'Finish Progate React Course',
-      completed: false,
-    },
-    {
-      id: 2,
-      title: 'Have lunch with Guru Domba',
-      completed: false,
-    },
-    {
-      id: 3,
-      title: 'Study React with Ninja Ken',
-      completed: false,
-    },
-  ]
+  {
+    id: 1,
+    title: 'Finish Progate React Course',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Have lunch with Guru Domba',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Study React with Ninja Ken',
+    completed: false,
+  },
+]
 
 function App() {
   const [todos, setTodos] = useState(data);
@@ -32,10 +32,14 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div>
-      <h1>My Todo List</h1>
-      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
     </div>
   )
 }
